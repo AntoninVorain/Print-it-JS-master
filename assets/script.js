6fd974aa438dc2ae9b717d64d50a6c8c48db8aa3
AntoninVorain <antoninvorain@gmail.com>
2023-05-08 16:55:31 +0200

    les boutons sont clicables

---
+++ assets/script.js
@@ -0,0 +1,94 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+const items = document.querySelectorAll('.banner-img');
+const dots = document.querySelectorAll('.dot')
+const nbSlide = items.length;
+let tagLine = document.querySelector('#tagline');
+const suivant = document.querySelector('.arrow_right');
+const precedent = document.querySelector('.arrow_left');
+let count = 0;
+
+dots.forEach((button, i) => {
+	button.addEventListener("click", () => {
+		// déselectionne les éléments
+		items.forEach((item) =>
+			item.classList.remove("active")
+		);
+		dots.forEach((button) =>
+			button.classList.remove("dot_selected")
+		);
+
+		items[i].classList.add("active");
+		button.classList.add("dot_selected");
+	});
+});
+
+// sélectionne le premier élément au chargement de la page
+items[0].classList.add("active");
+dots[0].classList.add("dot_selected");
+
+
+
+function slideSuivante(){
+	items[count].classList.remove('active');
+	dots[count].classList.remove('dot_selected');
+	if(count < nbSlide - 1){
+			count++;
+	} else {
+			count = 0;
+	}
+
+	items[count].classList.add('active');
+	dots[count].classList.add('dot_selected');
+	tagLine = slides[count].tagLine;
+	document.querySelector('#tagline').innerHTML = tagLine;
+	//console.log(count);
+	
+}
+suivant.addEventListener('click', slideSuivante)
+
+function slidePrecedente(){
+	items[count].classList.remove('active');
+	dots[count].classList.remove('dot_selected');
+	if(count > 0){
+			count--;
+	} else {
+			count = nbSlide - 1;
+	}
+
+	items[count].classList.add('active');
+	dots[count].classList.add('dot_selected');
+	tagLine = slides[count].tagLine;
+	document.querySelector('#tagline').innerHTML = tagLine;
+	// console.log(count);
+	
+}
+precedent.addEventListener('click', slidePrecedente)
+
+function keyPress(e){
+	//console.log(e);
+	
+	if(e.keyCode === 37){
+			slidePrecedente();
+	} else if(e.keyCode === 39){
+			slideSuivante();
+	}
+}
+document.addEventListener('keydown', keyPress)
